b372184b8bafd4fca6b5ebf4b90d76d6fca07862
Raghavendra Manjegowda <raghavendrahm0410@gmail.com>
2017-01-08 01:55:32 -0800

    adding Aureola conky

---
+++ Aureola/maintenance_change_version_ and_date.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+##################################################################################################################
+#
+# Current project : Conky aureola
+#
+# Source 	: 	https://github.com/erikdubois/Aureola
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Surfn icons
+# Colourfull and playfull icons
+# https://github.com/erikdubois/surfn
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+find .  -name "conky.conf" -type f -exec sed -i  's/v1.7.4/v1.7.5/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   's/v1.7.4/v1.7.5/g' {} \;
+find .  -name "conky.conf" -type f -exec sed -i  's/21\/12\/2016/02\/01\/2017/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   's/21\/12\/2016/02\/01\/2017/g' {} \;
