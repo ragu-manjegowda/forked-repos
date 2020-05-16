160fd6176b6a36c950888f62538d2cde5718ecbd
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2020-05-16 01:03:18 -0700

    [feat] Add thefuck submodule

--- thefuck/thefuck/rules/man_no_space.py
+++
@@ -1,9 +0,0 @@
-def match(command):
-    return (command.script.startswith(u'man')
-            and u'command not found' in command.stderr.lower())
-
-
-def get_new_command(command):
-    return u'man {}'.format(command.script[3:])
-
-priority = 2000
--- thefuck/thefuck/system/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-
-if sys.platform == 'win32':
-    from .win32 import *
-else:
-    from .unix import *
